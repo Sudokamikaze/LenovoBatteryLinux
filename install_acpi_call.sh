8aa8791fb16e281e6de0a6147c8a55be333bc149
pulshen <pulshencode@outlook.com>
2015-08-27 16:09:52 +0300

    Fixed permissions

--- install_acpi_call.sh
+++ install_acpi_call.sh
@@ -2,9 +2,9 @@
 sudo apt-get install dkms git build-essential gedit
 sudo apt-get install linux-headers-$(uname -r)
 git clone http://github.com/mkottman/acpi_call.git
-mkdir /usr/src/acpi_call-0.0.1
-cp -rp acpi_call/* /usr/src/acpi_call-0.0.1
-cp dkms.conf /usr/src/acpi_call-0.0.1
+sudo mkdir /usr/src/acpi_call-0.0.1
+sudo cp -rp acpi_call/* /usr/src/acpi_call-0.0.1
+sudo cp dkms.conf /usr/src/acpi_call-0.0.1
 sudo dkms add -m acpi_call -v 0.0.1
 sudo dkms build -m acpi_call -v 0.0.1
 sudo dkms install -m acpi_call -v 0.0.1
