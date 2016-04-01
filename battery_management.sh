ad182e40a1018cdbf50e64b2e34155b8da2268b2
sudokamikaze <pulshencode@outlook.com>
2016-04-01 18:37:33 +0300

    Updates to gramar

--- battery_management.sh
+++ battery_management.sh
@@ -6,16 +6,16 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 echo ==================================
-echo 1 Install battery charge level to 50% and lock
-echo 2 Install battery charge level to 100%
+echo "1 Install battery protection mode(Charge level about 55~59%)"
+echo "2 Disable battery protection mode(Charge level 100%)"
 echo ==================================
 echo -n "Choose an action: "
 read item
 case "$item" in
-    1)echo "Installing battery charge 50%"
+    1)echo "Installing battery protection mode"
       echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 4' > /proc/acpi/call
         ;;
-    2)echo "Installing battery charge 100%"
+    2)echo "Disabling battery protection mode and installing charge level to 100%"
       echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 5' > /proc/acpi/call
         ;;
     *) echo "Noting Entered"
