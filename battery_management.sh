699600c9c8f5efb54e9349dcb447c96593272ec2
Sudokamikaze <pulshencode@outlook.com>
2017-02-02 17:51:10 +0200

    Implement feature to charge battery to 60% and stop charge process

--- battery_management.sh
+++ battery_management.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-# Pulshen battery management
-# root checker
+
+chargelevel=$(cat /sys/class/power_supply/BAT1/capacity)
+
 if [ "x$(id -u)" != 'x0' ]; then
     echo 'Error: this script can only be executed by root'
     exit 1
 fi
+
+function charge_to_60 {
+if (("$chargelevel" => "60")); then
+  echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 4' > /proc/acpi/call
+  echo "Done, battery charged to $chargelevel"
+fi
+}
+
+echo "Current charge level: $chargelevel"
 echo ==================================
 echo "1 Install battery protection mode(Charge level about 55~59%)"
 echo "2 Disable battery protection mode(Charge level 100%)"
+echo "3 Charge battery to 60% and stop charge"
 echo ==================================
 echo -n "Choose an action: "
 read item
@@ -17,6 +28,14 @@ case "$item" in
         ;;
     2)echo "Disabling battery protection mode"
       echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 5' > /proc/acpi/call
+        ;;
+    3)
+    echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 5' > /proc/acpi/call
+    while [ "$loop" = "yes" ]
+    do
+    charge_to_60
+    sleep 1m
+    done
         ;;
     *) echo "Noting Entered"
         ;;
